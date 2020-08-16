13388637d3e2e7228947201b508418223db92563
Javier Aguilar <coyote176@msn.com>
2020-08-16 19:57:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   @@all = []
   
-  attr_accessor :name
+  attr_accessor :name :save
   
   def initialize(name)
     @name= name
@@ -17,8 +17,8 @@ def self.clear_all
    @@all.clear
 end 
 
-def print_all
-  
+def self.print_all
+  @@all.each {|k9| puts k9.name} 
 end
 
 def save
